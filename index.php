67745d61d69888f1464a423b8946dade86302d6f
Hendro Pramana Sinaga <h.sinaga22@gmail.com>
2013-10-25 15:17:42 +0700

    Update Basic Header

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<!doctype html>
 <html>
+<head>
+  <title>Tersesat Di Medan - KORUPSI</title>
+  <meta charset="utf-8" />
+</head>
+<body>
+  <div class="wrapping-nav-menu">
+    <nav>
+      <ul>
+        <li>Home</li><li>About</li><li>Guide</li><li>Member Area</li>
+      </ul>
+    </nav>
+  </div>
+</body>
+</html>
